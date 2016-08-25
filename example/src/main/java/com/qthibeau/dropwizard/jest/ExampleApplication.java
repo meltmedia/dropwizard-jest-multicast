e177c236fac0d8435da063e6bb67319fdade77cb
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 14:57:49 -0700

    Implement dropwizard application

--- example/src/main/java/com/qthibeau/dropwizard/jest/ExampleApplication.java
+++ example/src/main/java/com/qthibeau/dropwizard/jest/ExampleApplication.java
@@ -1,7 +1,26 @@
 package com.qthibeau.dropwizard.jest;
 
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
-public class ExampleApplication {
+public class ExampleApplication extends Application<ExampleConfiguration> {
+
+    public static void main( String[] args ) throws Exception {
+        new ExampleApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<ExampleConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(ExampleConfiguration configuration, Environment environment) {
+
+    }
+
 }
