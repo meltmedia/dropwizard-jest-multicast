ba8fe91111691705c8b5a5d93f559a228b61dcac
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 11:00:30 -0700

    Create builder for MulticastClient

--- bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastClient.java
+++ bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastClient.java
@@ -1,7 +1,64 @@
 package com.qthibeau.dropwizard.jest;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+
+import java.util.Collection;
+import java.util.Set;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
 public class MulticastClient {
+
+    private Set<JestClient> criticalClients;
+    private Set<JestClient> nonCriticalClients;
+
+    /* Make constructor private because we are following the builder pattern */
+    private MulticastClient() {};
+
+    public static class Builder {
+
+        private MulticastClient client;
+
+        public Builder() {
+            this.client = new MulticastClient();
+        }
+
+        public MulticastClient build() {
+            return this.client;
+        }
+
+        public Builder withConfigurations(Collection<MulticastConfiguration> configurations) {
+
+            configurations.forEach((MulticastConfiguration configuration) -> {
+
+                configuration.getDatabaseUrls().forEach((String url) -> {
+
+                    JestClientFactory clientFactory = new JestClientFactory();
+                    HttpClientConfig clientConfig = new HttpClientConfig.Builder(url)
+                            .connTimeout(configuration.getConnectionTimeout())
+                            .readTimeout(configuration.getReadTimeout())
+                            .maxTotalConnection(configuration.getMaxTotalConnections())
+                            .defaultMaxTotalConnectionPerRoute(configuration.getMaxTotalConnectionsPerRoute())
+                            .multiThreaded(true)
+                            .build();
+
+                    clientFactory.setHttpClientConfig(clientConfig);
+
+                    if (configuration.isCritical()) {
+                        client.criticalClients.add(clientFactory.getObject());
+                    }
+                    else {
+                        client.nonCriticalClients.add(clientFactory.getObject());
+                    }
+
+                });
+            });
+
+            return this;
+        }
+    }
+
 }
