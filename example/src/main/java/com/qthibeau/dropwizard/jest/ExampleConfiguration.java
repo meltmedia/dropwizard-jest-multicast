e00e7ff6817ed9b4d87b400f2c7fc7a5bed4bb87
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 14:44:00 -0700

    Finish configuration file

--- example/src/main/java/com/qthibeau/dropwizard/jest/ExampleConfiguration.java
+++ example/src/main/java/com/qthibeau/dropwizard/jest/ExampleConfiguration.java
@@ -1,7 +1,32 @@
 package com.qthibeau.dropwizard.jest;
 
+import io.dropwizard.Configuration;
+
+import java.util.List;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
-public class ExampleConfiguration {
+
+public class ExampleConfiguration extends Configuration {
+
+    private String template;
+    private List<MulticastConfiguration> elasticsearch;
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public List<MulticastConfiguration> getElasticsearch() {
+        return elasticsearch;
+    }
+
+    public void setElasticsearch(List<MulticastConfiguration> elasticsearch) {
+        this.elasticsearch = elasticsearch;
+    }
+
 }
