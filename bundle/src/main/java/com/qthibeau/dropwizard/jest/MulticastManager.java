9a496ee1441bd1b54fb17bd1ff9fc7792c18b63c
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 11:25:06 -0700

    Add client to client manager

--- bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastManager.java
+++ bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastManager.java
@@ -1,7 +1,18 @@
 package com.qthibeau.dropwizard.jest;
 
+import java.util.Collection;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
+
 public class MulticastManager {
+    private MulticastClient client;
+
+    public MulticastManager(Collection<MulticastConfiguration> configurations) {
+        this.client = new MulticastClient.Builder()
+                .withConfigurations(configurations)
+                .build();
+    }
+
 }
