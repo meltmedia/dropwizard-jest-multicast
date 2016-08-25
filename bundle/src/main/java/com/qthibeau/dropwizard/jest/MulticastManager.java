3e12e6b81358c6f83406886f2f3d0b5f5bb97542
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 11:28:37 -0700

    Implement dropwizard Managed interface on manager;

--- bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastManager.java
+++ bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastManager.java
@@ -1,18 +1,34 @@
 package com.qthibeau.dropwizard.jest;
 
+import io.dropwizard.lifecycle.Managed;
+
 import java.util.Collection;
 
 /**
  * Created by qthibeault on 8/25/16.
  */
 
-public class MulticastManager {
+public class MulticastManager implements Managed {
+    private Collection<MulticastConfiguration> configurations;
     private MulticastClient client;
 
     public MulticastManager(Collection<MulticastConfiguration> configurations) {
+        this.configurations = configurations;
+    }
+
+    public MulticastClient getClient() {
+        return this.client;
+    }
+
+    @Override
+    public void start() throws Exception {
         this.client = new MulticastClient.Builder()
                 .withConfigurations(configurations)
                 .build();
     }
 
+    @Override
+    public void stop() throws Exception {
+        this.client.shutdownClient();
+    }
 }
