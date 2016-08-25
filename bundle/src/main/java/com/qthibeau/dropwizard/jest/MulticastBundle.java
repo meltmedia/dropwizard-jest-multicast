a4853f681bc44466c798aa49955efd31dae39905
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 11:48:09 -0700

    Implement dropwizard ConfiguredBundle. Create interface for getting configuration. Create builder class'

--- bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastBundle.java
+++ bundle/src/main/java/com/qthibeau/dropwizard/jest/MulticastBundle.java
@@ -1,7 +1,61 @@
 package com.qthibeau.dropwizard.jest;
 
+import io.dropwizard.Configuration;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+import java.util.Collection;
+import java.util.function.Supplier;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
-public class MulticastBundle {
+
+public class MulticastBundle <C extends Configuration> implements ConfiguredBundle<C> {
+
+    public static interface ConfigurationAccessor<C extends Configuration> {
+        public Collection<MulticastConfiguration> getConfiguration(C configuration);
+    }
+
+    public static class Builder<C extends Configuration> {
+        MulticastBundle<C> bundle;
+
+        public Builder() {
+            this.bundle = new MulticastBundle<C>();
+        }
+
+        public Builder withConfiguration(ConfigurationAccessor<C> configurationAccessor) {
+            this.bundle.accessor = configurationAccessor;
+            return this;
+        }
+
+        public MulticastBundle<C> build() {
+            if(bundle.accessor == null) {
+                throw new IllegalStateException("Cannot build MulticastBundle without configuration");
+            }
+
+            return this.bundle;
+        }
+    }
+
+    private ConfigurationAccessor<C> accessor;
+    private Collection<MulticastConfiguration> multicastConfigurations;
+    private MulticastManager clientManager;
+    private Supplier<MulticastClient> clientSupplier;
+
+    @Override
+    public void run(C configuration, Environment environment) throws Exception {
+        this.multicastConfigurations = this.accessor.getConfiguration(configuration);
+        this.clientManager = new MulticastManager(this.multicastConfigurations);
+        this.clientSupplier = clientManager::getClient;
+
+        environment.lifecycle().manage(this.clientManager);
+    }
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+
+    }
+
 }
