57ac21354909c7776adcd217be8169057ccb75ab
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 13:12:56 -0700

    Create unit tests for testing the Client builder class

---
+++ bundle/src/test/java/com/qthibeau/dropwizard/jest/MulticastClientBuilderTest.java
@@ -0,0 +1,89 @@
+package com.qthibeau.dropwizard.jest;
+
+import io.searchbox.client.JestClient;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Created by qthibeault on 8/25/16.
+ */
+
+public class MulticastClientBuilderTest {
+
+    private MulticastClient client;
+
+    @Test
+    public void emptyBuilderTest() {
+        client = new MulticastClient.Builder()
+                .build();
+
+        assertThat(client.getCriticalClients(), is(empty()));
+        assertThat(client.getNonCriticalClients(), is(empty()));
+    }
+
+    @Test
+    public void criticalClientBuilderTest() {
+        List<MulticastConfiguration> clientConfigurations = new LinkedList<>();
+
+        MulticastConfiguration clientConfiguration1 = new MulticastConfiguration();
+        clientConfiguration1.setClusterName("elasticsearch");
+        clientConfiguration1.setDatabaseUrls(Arrays.asList("http://localhost:9200", "http://localhost:9201"));
+        clientConfiguration1.setCritical(true);
+        clientConfigurations.add(clientConfiguration1);
+
+        MulticastClient client = new MulticastClient.Builder()
+                .withConfigurations(clientConfigurations)
+                .build();
+
+        assertThat(client.getCriticalClients(), hasSize(2));
+        assertThat(client.getNonCriticalClients(), is(empty()));
+    }
+
+    @Test
+    public void nonCriticalClientBuilderTest() {
+        List<MulticastConfiguration> clientConfigurations = new LinkedList<>();
+
+        MulticastConfiguration clientConfiguration1 = new MulticastConfiguration();
+        clientConfiguration1.setClusterName("elasticsearch");
+        clientConfiguration1.setDatabaseUrls(Arrays.asList("http://localhost:9200", "http://localhost:9201"));
+        clientConfigurations.add(clientConfiguration1);
+
+        MulticastClient client = new MulticastClient.Builder()
+                .withConfigurations(clientConfigurations)
+                .build();
+
+        assertThat(client.getNonCriticalClients(), hasSize(2));
+        assertThat(client.getCriticalClients(), is(empty()));
+    }
+
+    @Test
+    public void bothClientTypeBuilderTest() {
+        List<MulticastConfiguration> clientConfigurations = new LinkedList<>();
+
+        MulticastConfiguration clientConfiguration1 = new MulticastConfiguration();
+        clientConfiguration1.setClusterName("elasticsearch");
+        clientConfiguration1.setDatabaseUrls(Arrays.asList("http://localhost:9200", "http://localhost:9201"));
+        clientConfigurations.add(clientConfiguration1);
+
+
+        MulticastConfiguration clientConfiguration2 = new MulticastConfiguration();
+        clientConfiguration2.setClusterName("elasticsearch");
+        clientConfiguration2.setDatabaseUrls(Arrays.asList("http://localhost:9200", "http://localhost:9201"));
+        clientConfiguration2.setCritical(true);
+        clientConfigurations.add(clientConfiguration2);
+
+        MulticastClient client = new MulticastClient.Builder()
+                .withConfigurations(clientConfigurations)
+                .build();
+
+        assertThat(client.getNonCriticalClients(), hasSize(2));
+        assertThat(client.getCriticalClients(), hasSize(2));
+    }
+
+}
