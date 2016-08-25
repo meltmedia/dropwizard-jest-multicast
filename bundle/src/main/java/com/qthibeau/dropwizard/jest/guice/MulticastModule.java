8d7231262faef1ec17ee6f3cb1c5e4643ae22897
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-08-25 12:53:21 -0700

    Implement abtract module interface to provide a multicast client

--- bundle/src/main/java/com/qthibeau/dropwizard/jest/guice/MulticastModule.java
+++ bundle/src/main/java/com/qthibeau/dropwizard/jest/guice/MulticastModule.java
@@ -1,7 +1,39 @@
 package com.qthibeau.dropwizard.jest.guice;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.qthibeau.dropwizard.jest.MulticastBundle;
+import com.qthibeau.dropwizard.jest.MulticastClient;
+
+import java.util.function.Supplier;
+
 /**
  * Created by qthibeault on 8/25/16.
  */
-public class MulticastModule  {
+public class MulticastModule extends AbstractModule {
+
+    private MulticastBundle<?> bundle;
+
+    public MulticastModule( MulticastBundle<?> bundle ) {
+        this.bundle = bundle;
+    }
+
+    @Override
+    protected void configure() {
+        // no op
+    }
+
+    @Provides
+    @Singleton
+    public MulticastClient provideClient() {
+        return this.bundle.getClientSupplier().get();
+    }
+
+    @Provides
+    @Singleton
+    public Supplier<MulticastClient> provideClientSupplier() {
+        return this.bundle.getClientSupplier();
+    }
+
 }
