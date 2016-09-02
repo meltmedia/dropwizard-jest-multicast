f291d647c8d6a7bc0637a080d5a96bbab8994d40
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-09-02 11:10:54 -0700

    Update healthcheck to take multicast provider

--- bundle/src/main/java/com/meltmedia/dropwizard/jestmulticast/MulticastHealthcheck.java
+++ bundle/src/main/java/com/meltmedia/dropwizard/jestmulticast/MulticastHealthcheck.java
@@ -12,21 +12,21 @@ import java.util.function.Supplier;
  * Created by qthibeault on 9/2/16.
  */
 public class MulticastHealthcheck extends HealthCheck {
-    protected Supplier<JestClient> clientSupplier;
-    public MulticastHealthcheck( Supplier<JestClient> clientSupplier ) {
+    protected Supplier<MulticastClient> clientSupplier;
+    public MulticastHealthcheck( Supplier<MulticastClient> clientSupplier ) {
         this.clientSupplier = clientSupplier;
     }
 
     @Override
     protected Result check() throws Exception {
-        MulticastClient client = (MulticastClient) this.clientSupplier.get();
+        MulticastClient client = this.clientSupplier.get();
         List<JestResult> results = client.checkHealth(new Health.Builder().build());
         for(JestResult result : results) {
             if(!result.isSucceeded()){
                 return Result.unhealthy("Cannot get health for cluster");
             }
         }
-        
+
         // If all results succeeded then assume everything is healthy
         return Result.healthy();
     }
