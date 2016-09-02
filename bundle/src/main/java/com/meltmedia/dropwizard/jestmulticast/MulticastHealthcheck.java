e8e346e693eecaa00af97178305f878399d88a49
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-09-02 10:17:27 -0700

    Create class to register with dropwizard for the healthcheck

---
+++ bundle/src/main/java/com/meltmedia/dropwizard/jestmulticast/MulticastHealthcheck.java
@@ -0,0 +1,33 @@
+package com.meltmedia.dropwizard.jestmulticast;
+
+import com.codahale.metrics.health.HealthCheck;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestResult;
+import io.searchbox.cluster.Health;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Created by qthibeault on 9/2/16.
+ */
+public class MulticastHealthcheck extends HealthCheck {
+    protected Supplier<JestClient> clientSupplier;
+    public MulticastHealthcheck( Supplier<JestClient> clientSupplier ) {
+        this.clientSupplier = clientSupplier;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        MulticastClient client = (MulticastClient) this.clientSupplier.get();
+        List<JestResult> results = client.checkHealth(new Health.Builder().build());
+        for(JestResult result : results) {
+            if(!result.isSucceeded()){
+                return Result.unhealthy("Cannot get health for cluster");
+            }
+        }
+        
+        // If all results succeeded then assume everything is healthy
+        return Result.healthy();
+    }
+}
